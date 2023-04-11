62b6e043da3e567556ace3535c629d8dde94cf6b
maikdonut <73718190+maikdonut@users.noreply.github.com>
2023-04-11 23:17:24 +0300

    Create __init__.py

--- sim/sim/agents/__init__.py
+++ sim/sim/agents/__init__.py
@@ -1,6 +1,3 @@
-#from .recommender import Recommender
-#from .dummy import DummyRecommender
-#from .remote import RemoteRecommender
 from sim.agents.recommender import Recommender
 from sim.agents.dummy import DummyRecommender
 from sim.agents.remote import RemoteRecommender
